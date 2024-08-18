1f987b1263d693233d75eac163b4e8be4cf8b139
Mateusz Ormianek <mattormianek@gmail.com>
2024-08-18 09:32:20 +0200

    style: formatted and pylinted

--- format_between_lines.py
+++ format_between_lines.py
@@ -1,37 +1,51 @@
+"""Formats and lints specific lines in a Python file."""
+
+# pylint: disable=unspecified-encoding
 import subprocess
 import argparse
 
+
 def extract_lines(file_path, start_line, end_line):
-    with open(file_path, 'r') as file:
+    """Extracts the lines between the given start and end line numbers."""
+    with open(file_path, "r") as file:
         lines = file.readlines()
-    return lines[start_line-1:end_line]
+    return lines[start_line - 1 : end_line]
+
 
 def format_with_black(code):
+    """Formats the given code using black."""
     result = subprocess.run(
-        ['black', '-', '-l', '100'],
+        ["black", "-", "-l", "100"],
         input="".join(code),
         text=True,
+        check=True,
         capture_output=True,
     )
     return result.stdout
 
+
 def lint_with_pylint(code):
-    with open('temp_code.py', 'w') as temp_file:
+    """Lints the given code using pylint."""
+    with open("temp_code.py", "w") as temp_file:
         temp_file.writelines(code)
 
-    subprocess.run(['pylint', 'temp_code.py'])
-    subprocess.run(['rm', 'temp_code.py'])
+    subprocess.run(["pylint", "temp_code.py"], check=True)
+    subprocess.run(["rm", "temp_code.py"], check=True)
+
 
 def replace_lines(file_path, start_line, end_line, new_code):
-    with open(file_path, 'r') as file:
+    """Replaces the lines between the given start and end line numbers with the given new code."""
+    with open(file_path, "r") as file:
         lines = file.readlines()
 
-    lines[start_line-1:end_line] = new_code
+    lines[start_line - 1 : end_line] = new_code
 
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         file.writelines(lines)
 
+
 def main(file_path, start_line, end_line):
+    """Main function that formats and lints specific lines in a Python file."""
     code_to_process = extract_lines(file_path, start_line, end_line)
 
     formatted_code = format_with_black(code_to_process)
@@ -40,6 +54,7 @@ def main(file_path, start_line, end_line):
 
     replace_lines(file_path, start_line, end_line, formatted_code.splitlines(keepends=True))
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Format and lint specific lines in a Python file.")
     parser.add_argument("file_path", type=str, help="Path to the Python file.")
@@ -47,5 +62,4 @@ if __name__ == "__main__":
     parser.add_argument("end_line", type=int, help="Ending line number (inclusive, 1-indexed).")
 
     args = parser.parse_args()
-    print(args)
     main(args.file_path, args.start_line, args.end_line)
