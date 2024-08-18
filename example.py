df2bb3f2a07e6b8d24c8e15e71da2b9dc7e49d87
Mateusz Ormianek <mattormianek@gmail.com>
2024-08-18 23:05:18 +0200

    docs: updated example for more advanced code

--- example.py
+++ example.py
@@ -1,13 +1,32 @@
 """Example module."""
 import subprocess
 
+def two_sum_better(nums: list, target: int) -> list:
+    num_to_index = {}
+    for index, number in enumerate(nums):
+        complement = target - number
+        if complement in num_to_index:
+            return [num_to_index[complement], index]
+        num_to_index[number] = index
 
-def format_with_black(code):
-    """Formats the given code using black."""
-    result = subprocess.run(
-        ["black", "-"],
-        input="".join(code),
-        text=True,
-        capture_output=True,
-    )
-    return result.stdout
+def test_function(f):
+    nums = [2, 7, 11, 15]
+    target = 9
+    assert f(nums, target) == [0, 1], "Test case 1 failed"
+
+    nums = [3, 2, 4]
+    target = 6
+    assert f(nums, target) == [1, 2], "Test case 2 failed"
+
+    nums = [3, 3]
+    target = 6
+    assert f(nums, target) == [0, 1], "Test case 3 failed"
+
+    nums = [1, 2]
+    target = 10
+    assert f(nums, target) is None, "Test case 4 failed"
+
+    nums = [10, 11, 18, 19]
+    target = 37
+    assert f(nums, target) == [2, 3], "Test case 5 failed"
+test_function(two_sum_better)
